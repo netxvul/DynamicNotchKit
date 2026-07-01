84d49ea212d308d673c6d10479761348301776d8
netxvul <netxvul@noreply.gitcode.com>
2026-07-01 15:10:29 +0800

    Remove shadow for floating style

--- Sources/DynamicNotchKit/Views/NotchContentView.swift
+++ Sources/DynamicNotchKit/Views/NotchContentView.swift
@@ -18,6 +18,7 @@ struct NotchContentView<Expanded, CompactLeading, CompactTrailing>: View where E
     }
 
     private var shadowOpacity: CGFloat {
+        if style.isFloating { return 0 }
         if dynamicNotch.hoverBehavior.contains(.increaseShadow), dynamicNotch.isHovering {
             return 0.8
         } else if dynamicNotch.state != .expanded {
